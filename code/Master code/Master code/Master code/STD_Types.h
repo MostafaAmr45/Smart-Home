d23d4349bd7021e63dc298ebb6016e6e442f951c
MostafaAmr45 <43935726+MostafaAmr45@users.noreply.github.com>
2021-09-30 15:52:00 +0200

    add some files

---
+++ code/Master code/Master code/Master code/STD_Types.h
@@ -0,0 +1,19 @@
+/*
+* STD_Types.h
+*
+* Created: 8/25/2019 11:16:48 AM
+*  Author: Mostafa Amr & Mohamed Zaghlol
+*/
+
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+
+typedef unsigned char uint8;
+typedef signed char	  sint8;
+typedef unsigned short uint16;
+typedef signed short sint16;
+typedef double float64;
+typedef unsigned long uint32;
+
+#endif /* STD_TYPES_H_ */
